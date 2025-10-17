a39daf6c5dc682e7388564b31ef646b8afd61900
Robyn <downie.robyn@hotmail.co.uk>
2025-10-17 13:47:30 -0700

    update local host

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,8 @@
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 import { fileURLToPath } from 'url'
-import { dirname, resolve } from 'path'
 
 const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
 
 // https://vite.dev/config/
 export default defineConfig({
